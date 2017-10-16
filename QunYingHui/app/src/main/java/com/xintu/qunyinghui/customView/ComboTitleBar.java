df60594ad0fca65170dd48ccdc874a3fe964c74b
tongcf <tong_chf@163.com>
2017-10-16 17:28:57 +0800

    create combo view

---
+++ QunYingHui/app/src/main/java/com/xintu/qunyinghui/customView/ComboTitleBar.java
@@ -0,0 +1,25 @@
+package com.xintu.qunyinghui.customView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by Tong on 2017/10/16.
+ */
+
+public class ComboTitleBar extends RelativeLayout {
+
+    public ComboTitleBar(Context context) {
+        this(context, null);
+    }
+
+    public ComboTitleBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ComboTitleBar(Context context, AttributeSet attrs, int
+            defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
